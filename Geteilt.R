112cb742b6dd0e3d0378ae685a5e678090491220
LachlanJuraj <lachlan.eckardt@posteo.de>
2026-06-18 16:02:54 +0200

    df cntry

---
+++ Geteilt.R
@@ -0,0 +1,6 @@
+df <- data.frame(
+  cntry = c("AU", "AT", "BE", "BG", "CA", "CZ", "DK", "EE", "FI", "FR", "DE", 
+            "GR", "HU", "IS", "IE", "IT", "JP", "LV", "LT", "LU", "MT", "NL", 
+            "NZ", "NO", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "CH", "GB", 
+            "US")
+)
