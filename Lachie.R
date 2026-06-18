7c357875a83bc02fdaba8aa6ce7af846f52dc8cb
LachlanJuraj <lachlan.eckardt@posteo.de>
2026-06-18 16:23:22 +0200

    BG BE ausgetauscht

--- Lachie.R
+++ Lachie.R
@@ -5,7 +5,7 @@ Lachie_df <- data.frame(
   lachie_col =c(1:34)
 )
 Lachie_df <- Lachie_df %>% 
-mutate(cntry = c("AU", "AT", "BE", "BG", "CA", "CZ", "DK", "EE", "FI", "FR", "DE", 
+mutate(cntry = c("AU", "AT", "BG", "BE", "CA", "CZ", "DK", "EE", "FI", "FR", "DE", 
           "GR", "HU", "IS", "IE", "IT", "JP", "LV", "LT", "LU", "MT", "NL", 
           "NZ", "NO", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "CH", "GB", 
           "US"))
